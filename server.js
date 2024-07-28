f228f4452c00353a83158c11725fe21102fe58d4
Adwait Deshpande <adwaitdeshpande.and@gmail.com>
2024-07-28 10:31:39 +0530

    Solved

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 'use strict';
-require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var expect      = require('chai').expect;
+var cors        = require('cors');
 
-const app = express();
+var apiRoutes         = require('./routes/api.js');
+var fccTestingRoutes  = require('./routes/fcctesting.js');
+var runner            = require('./test-runner');
+
+var app = express();
+
+const helmet = require('helmet')
+
+app.use(helmet.frameguard())
+app.use(helmet.dnsPrefetchControl())
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -22,7 +29,7 @@ app.route('/b/:board/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/board.html');
   });
-app.route('/b/:board/:threadid')
+app.route('/b/:board/:thread_id')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/thread.html');
   });
@@ -39,6 +46,9 @@ fccTestingRoutes(app);
 //Routing for API 
 apiRoutes(app);
 
+//Sample Front-end
+
+    
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
@@ -47,16 +57,17 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
       } catch(e) {
-        console.log('Tests are not valid:');
-        console.error(e);
+        var error = e;
+          console.log('Tests are not valid:');
+          console.log(error);
       }
     }, 1500);
   }
